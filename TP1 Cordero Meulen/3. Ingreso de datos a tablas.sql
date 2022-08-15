5fe6c66492cd72cb7ac11ef2da0f6af8664f3ca3
MeulenCordero <106438887+MeulenCordero@users.noreply.github.com>
2022-08-15 11:19:10 -0300

    Update 3. Ingreso de datos a tablas.sql

--- TP1 Cordero Meulen/3. Ingreso de datos a tablas.sql
+++ TP1 Cordero Meulen/3. Ingreso de datos a tablas.sql
@@ -8,18 +8,18 @@ INSERT INTO perro VALUES(14,  "Fido",  '2012-11-12' , 'F', 40546935);
 INSERT INTO perro VALUES(15, "Shuman", '2017-03-11', 'M', 23546987);
 INSERT INTO perro VALUES(16, "Frida", '2019-05-21', 'F', 28957346);
 
-INSERT INTO historial VALUE(1, '2018-09-15', 13, "Peluqueria", 2350.00);
-INSERT INTO historial VALUE(2, '2018-11-16', 13, "Atención Clínica", 5960.00);
-INSERT INTO historial VALUE(3, '2022-07-12', 13, "Castración", 23000.00);
+INSERT INTO historial VALUES(1, '2018-09-15', 13, "Peluqueria", 2350.00);
+INSERT INTO historial VALUES(2, '2018-11-16', 13, "Atención Clínica", 5960.00);
+INSERT INTO historial VALUES(3, '2022-07-12', 13, "Castración", 23000.00);
 
-INSERT INTO historial VALUE(4, '2022-07-15', 14, "Peluqueria", 3540.00);
-INSERT INTO historial VALUE(5, '2022-07-25', 14, "Atención Clínica", 6500.00);
-INSERT INTO historial VALUE(6, '2022-08-05', 14, "Castración", 21500.00);
+INSERT INTO historial VALUES(4, '2022-07-15', 14, "Peluqueria", 3540.00);
+INSERT INTO historial VALUES(5, '2022-07-25', 14, "Atención Clínica", 6500.00);
+INSERT INTO historial VALUES(6, '2022-08-05', 14, "Castración", 21500.00);
 
-INSERT INTO historial VALUE(7, '2021-05-20', 15, "Peluqueria", 4300.00);
-INSERT INTO historial VALUE(8, '2021-07-25', 15, "Atención Clínica", 7600.00);
-INSERT INTO historial VALUE(9, '2022-07-05', 15, "Castración", 27000.00);
+INSERT INTO historial VALUES(7, '2021-05-20', 15, "Peluqueria", 4300.00);
+INSERT INTO historial VALUES(8, '2021-07-25', 15, "Atención Clínica", 7600.00);
+INSERT INTO historial VALUES(9, '2022-07-05', 15, "Castración", 27000.00);
 
-INSERT INTO historial VALUE(10, '2021-06-10', 16, "Castración", 7300.00);
-INSERT INTO historial VALUE(11, '2021-08-15', 16, "Atención Clínica", 7400.00);
-INSERT INTO historial VALUE(12, '2022-03-05', 16, "Peluqueria", 17000.00);
+INSERT INTO historial VALUES(10, '2021-06-10', 16, "Castración", 7300.00);
+INSERT INTO historial VALUES(11, '2021-08-15', 16, "Atención Clínica", 7400.00);
+INSERT INTO historial VALUES(12, '2022-03-05', 16, "Peluqueria", 17000.00);
